265504b4c931237eec22043d109e5b489423a1aa
santoshahirwar-commits <santosh.ahirwar@gmail.com>
2026-08-25 20:53:12 +0530

    code error

--- environment/preprod/main.tf
+++ environment/preprod/main.tf
@@ -1,22 +1,21 @@
 module "rg" {
-    source = "../environment/preprod/azurerm_resource_group"
+    source = "../../module/azurerm_resource_group"
     rg=var.rg
     }
 
-
 module "stg" {
     depends_on = [module.rg]
-    source = "../environment/preprod/azurerm_storage_account"
+    source = "../../module/azurerm_storage_account"
     stg=var.stg
   
 }
 module "vnet" {
     depends_on = [module.rg]
-    source = "../environment/preprod/azurerm_virtual_network"
+    source = "../../module/azurerm_virtual_network"
     vnet=var.vnet
   
 }
 module "snet" {
-    source = "../environment/preprod/azurerm_subnet"
+    source ="../../module/azurerm_subnet"
     snet =var.snet
   }
